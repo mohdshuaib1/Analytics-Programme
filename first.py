c42453dcec6882f1a95d09b9456bc7ac46238f71
mohdshuaib1 <83017529+mohdshuaib1@users.noreply.github.com>
2021-06-06 14:56:20 +0530

    Programme Zero for Beginners

---
+++ first.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  6 14:41:04 2021
+
+@author: MOHD SHUAIB
+"""
+
+x = [12,14,16,18]
+y = [20,40,60,80]
+print(x)
+print(y)
+import matplotlib.pyplot as plt
+plt.plot(x,y)
